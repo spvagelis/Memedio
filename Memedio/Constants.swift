404409419058b7de310b29a7fcd2ceaaff8e463d
spvagelis <spirou.vagelis@gmail.com>
2021-02-15 14:39:08 +0200

    The two text fields can pan, rotate and pinch. Added constraints programmatically. Save the memes in table view and collection view. Can change the font.

---
+++ Memedio/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Memedio
+//
+//  Created by vagelis spirou on 3/1/21.
+//
+
+import UIKit
+
+struct Constants {
+    
+    static let topTextFieldText = "ENTER TOP TEXT"
+    static let bottomTextFieldText = "ENTER BOTTOM TEXT"
+    static let fontOfTextField = "HelveticaNeue-CondensedBlack"
+    static let initialSelectedMemeItemNumber = -1
+    static let defaultSizeOfTextFields: CGFloat = 40.0
+    
+}
